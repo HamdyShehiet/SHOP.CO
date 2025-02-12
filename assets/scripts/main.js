2c03c2c6ef470e0381c51766ce5149559bb115ab
Hamdy <tantaacademyt196@gmail.com>
2025-02-12 23:34:46 +0000

    Add: Header Menu Functionality & Search Field Functionality

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,15 @@
 document.getElementById("close-banar").onclick = ()=>{
     document.getElementById("banar-sign-up").style.display = "none";
 }
+
+document.getElementById("menu-icon").onclick = () => {
+    if(document.getElementById("menu").classList.contains("menu")){
+        document.getElementById("menu").classList.remove("menu")
+    }else{
+        document.getElementById("menu").classList.add("menu")
+    }
+}
+
+document.getElementById("search-field").onclick = ()=>{
+    document.getElementById("search-box").classList.toggle("append-field");
+}
